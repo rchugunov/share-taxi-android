3bec8abac1296311a57eb90ad9e9365f43ac3296
Roman Chugunov (Home macbook) <chugunov.r@gmail.com>
2018-12-30 13:48:28 +0300

    removed all mentiones of old package

--- app/src/main/java/com/github/sharetaxi/Constants.kt
+++ app/src/main/java/com/github/sharetaxi/Constants.kt
@@ -3,7 +3,7 @@ package com.github.sharetaxi
 object Constants {
     const val SUCCESS_RESULT = 0
     const val FAILURE_RESULT = 1
-    const val PACKAGE_NAME = "com.github.rchugunov.weather"
+    const val PACKAGE_NAME = "com.github.sharetaxi"
     const val RECEIVER = "$PACKAGE_NAME.RECEIVER"
     const val RESULT_DATA_KEY = "$PACKAGE_NAME.RESULT_DATA_KEY"
     const val LOCATION_DATA_EXTRA = "$PACKAGE_NAME.LOCATION_DATA_EXTRA"
